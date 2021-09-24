cec59bc109cd528a47f6a8d16d14c9ad3a822f1f
DragisaDraskovic <dragisadraskovic2@gmail.com>
2021-09-24 02:55:02 +0200

    Vezbe 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './vozilo/vozilo.component';
+import { AutomobilComponent } from './vozilo/automobil/automobil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
