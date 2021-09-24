741d70dc5e86d0a8caeb115f23388ddaeb0a9769
DragisaDraskovic <dragisadraskovic2@gmail.com>
2021-09-24 03:02:42 +0200

    vezbe 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 import { AppComponent } from './app.component';
 import { VoziloComponent } from './vozilo/vozilo.component';
 import { AutomobilComponent } from './vozilo/automobil/automobil.component';
+import { NacionalnostComponent } from './nacionalnost/nacionalnost.component';
+import { LigaComponent } from './liga/liga.component';
+import { TimComponent } from './tim/tim.component';
+import { IgracComponent } from './igrac/igrac.component';
+import { HomeComponent } from './core/home/home.component';
+import { AuthorComponent } from './core/author/author.component';
+import { AboutComponent } from './core/about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     VoziloComponent,
-    AutomobilComponent
+    AutomobilComponent,
+    NacionalnostComponent,
+    LigaComponent,
+    TimComponent,
+    IgracComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
